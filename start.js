6d3945d5932f8c3c5f0c752e4f462857aa6e7109
Zeeshan ATLC <MuhammadZeeshan2>
2023-10-03 00:18:05 +0500

    index.js

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 // Config file to launch our application
 
-const app = require('./app');
+const app = require('./index');
 
 const server = app.listen(3000, () => {
     console.log(`Express app running on port ${server.address().port}`)
